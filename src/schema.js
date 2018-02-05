c5b5cbce69eff3db7d7373c8a7c23d6ce87a2b7d
seventh1976 <seventh1976@gmail.com>
2018-02-05 17:12:02 +0800

    Adding Author Type

--- src/schema.js
+++ src/schema.js
@@ -13,3 +13,13 @@ let {
   // This is the class we need to create the schema
   GraphQLSchema,
 } = require('graphql');
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent an author",
+  fields: () => ({
+    id: {type: new GraphQLNonNull(GraphQLString)},
+    name: {type: new GraphQLNonNull(GraphQLString)},
+    twitterHandle: {type: GraphQLString}
+  })
+});
