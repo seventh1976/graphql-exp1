494f62fd7bb656e85f6c5549266e7054aa8c86f1
seventh1976 <seventh1976@gmail.com>
2018-02-05 17:19:37 +0800

    Create PostType object

--- src/schema.js
+++ src/schema.js
@@ -23,3 +23,19 @@ const AuthorType = new GraphQLObjectType({
     twitterHandle: {type: GraphQLString}
   })
 });
+
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  description: "This represent a Post",
+  fields: () => ({
+    id: {type: new GraphQLNonNull(GraphQLString)},
+    title: {type: new GraphQLNonNull(GraphQLString)},
+    body: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve: function(post) {
+        return _.find(Authors, a => a.id == post.author_id);
+      }
+    }
+  })
+});
