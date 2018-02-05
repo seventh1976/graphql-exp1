3c14af9c405ef6046e62cf74ea93348adc44660f
seventh1976 <seventh1976@gmail.com>
2018-02-05 18:54:01 +0800

    add express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./src/schema.js');
+
+let post = 3000;
+const app = express();
+
+app.use('/', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
+
+app.listen(port);
+console.log('GraphQL API server running at localhost:'+ port);
